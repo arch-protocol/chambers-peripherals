a62212e0deb2e439397ad1608d81734d0c49d665
Juan Pablo Schele <juan@archfinance.io>
2024-02-26 14:09:40 -0300

    💚 fix build

--- src/TradeIssuerV3.sol
+++ src/TradeIssuerV3.sol
@@ -81,7 +81,7 @@ contract TradeIssuerV3 is ITradeIssuerV3, Ownable, ReentrancyGuard {
     /**
      * @param _wrappedNativeToken        Wrapped network native token
      */
-    constructor(address _wrappedNativeToken, address _chamberGod) Ownable(msg.sender) {
+    constructor(address _wrappedNativeToken, address _chamberGod) Ownable() {
         wrappedNativeToken = _wrappedNativeToken;
         chamberGod = IChamberGod(_chamberGod);
     }
