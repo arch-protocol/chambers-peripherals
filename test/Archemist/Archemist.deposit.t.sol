8e248829906097ae5afa345d76add74def1d6a6d
Juan Pablo Schele <juan@archfinance.io>
2024-02-29 10:10:56 -0300

    forge fmt

--- test/Archemist/Archemist.deposit.t.sol
+++ test/Archemist/Archemist.deposit.t.sol
@@ -6,7 +6,6 @@ import { IArchemist } from "src/interfaces/IArchemist.sol";
 import { Pausable } from "@openzeppelin/contracts/utils/Pausable.sol";
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-
 contract ArchemistDepositTest is ArchemistTest {
     /*//////////////////////////////////////////////////////////////
                               REVERT
